a1aac0f70272690dbfc0e9491deb7f49c7b9ca9b
tanjie <tanjie@plusyoou.com>
2017-02-15 18:05:19 +0800

    logincheck调通

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -1,3 +1,14 @@
 $(function(){
 	$("#head_included").load("head.html")
 })
+
+//取URL后的参数 function GetQueryString(name)
+//URL的参数&参数名1=XXXX&参数名2=XXXX&参数名3=XXXX  
+//alert(GetQueryString("参数名2"));
+function GetQueryString(name) {
+var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+var r = window.location.search.substr(1).match(reg);
+if (r != null)
+    return unescape(r[2]);
+return null;
+}
